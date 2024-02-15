33a9395d96d27f023cd094c85b6bf28472d62e31
karthiksalanki <134361582+karthiksalanki@users.noreply.github.com>
2024-02-15 20:19:23 +0530

    removed rest_framework from settings

--- user_log/settings.py
+++ user_log/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'import_export',
     'rest_framework',
     'user_logapp',
     'django_extensions',
